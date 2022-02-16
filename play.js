0b5590faf375da8999099ab694847a8fceb02925
Noah <lauffern@gmail.com>
2022-02-16 18:37:42 +0000

    Update ES6 shorthand exports

--- play.js
+++ play.js
@@ -1,24 +1,8 @@
-const net = require("net");
 
-// establishes a connection with the game server
-const connect = function () {
-  const conn = net.createConnection({
-    host: 'localhost',
-    port: 50541,
-  });
+const { connect } = require('./client')
 
-  // interpret incoming data as text
-  conn.setEncoding("utf8");
-
-  conn.on('data', (data) => {
-    console.log(data.toString());
-  
-  });
-
-
-
-  return conn;
-};
 
 console.log("Connecting ...");
 connect();
+ 
+
