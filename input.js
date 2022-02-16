5b91ca65c90a8bc8fd9e3cc026e0b5ed8e13340b
Noah <lauffern@gmail.com>
2022-02-16 20:10:16 +0000

    make him move!

--- input.js
+++ input.js
@@ -1,11 +1,7 @@
-const handleUserInput = function (data) {
-  // your code here
-  if (data === '\u0003') {
-    process.exit();
-  }
-};
+let connection;
 
-const setupInput = function () {
+const setupInput = function (conn) {
+  connection = conn
   const stdin = process.stdin;
   stdin.setRawMode(true);
   stdin.setEncoding("utf8");
@@ -15,4 +11,25 @@ const setupInput = function () {
 };
 
 
-module.exports = { setupInput }
+const handleUserInput = function (data) {
+  // your code here
+  if (data === '\u0003') {
+    process.exit();
+  }
+  if (data === 'w' ) {
+    connection.write('Move: up');
+  }
+  if (data === 'a') {
+    connection.write('Move: left');
+  }
+  if (data === 's') {
+    connection.write('Move: down');
+  }
+  if (data === 'd') {
+    connection.write('Move: right');
+  }
+
+};
+
+
+module.exports = { setupInput } 
--- play.js
+++ play.js
@@ -4,7 +4,8 @@ const { setupInput } = require("./input");
 
 
 console.log("Connecting ...");
-connect();
-setupInput()
+
+setupInput(connect());
+
  
 
